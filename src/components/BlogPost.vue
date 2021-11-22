de5f764ba5b722ae9f9f8363aabc3fd261a29497
swaubhik <b18csel299@cit.ac.in>
2021-11-22 15:16:28 +0530

    🔥 edited

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -10,7 +10,7 @@
           Login/Register<Arrow class="arrow arrow-light" />
         </router-link>
         <router-link class="link" v-else :to="{ name: 'ViewBlog', params: { blogid: this.post.blogID } }">
-          View The Post<Arrow class="arrow" />
+          View The Lesson<Arrow class="arrow" />
         </router-link>
       </div>
     </div>
