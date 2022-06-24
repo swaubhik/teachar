09d05ccd683402d4df2e0e64c5a27d5805f2280b
Swaubhik Chakraborty <b18csel299@cit.ac.in>
2022-06-24 15:33:19 +0530

    ✨ NEW FEATURE: Create Accordion.vue

---
+++ src/components/Accordion.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <div @click="toggleAccordion()" class="flex items-center space-x-3" :aria-expanded="isOpen" :aria-controls="`collapse${_uid}`">
+      <slot name="title" />
+      <svg
+        class="w-3 transition-all duration-200 transform"
+        :class="{
+          'rotate-180': isOpen,
+          'rotate-0': !isOpen,
+        }"
+        fill="none"
+        stroke="currentColor"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 16 10"
+        aria-hidden="true"
+      >
+        <path d="M15 1.2l-7 7-7-7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+      </svg>
+    </div>
+
+    <div v-show="isOpen" :id="`collapse${_uid}`">
+      <slot name="content" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+
+  methods: {
+    toggleAccordion() {
+      this.isOpen = !this.isOpen;
+    },
+  },
+};
+</script>
+<style scoped>
+.flex {
+  display: flex;
+}
+.items-center {
+  align-items: center;
+}
+.w-3 {
+  width: 0.75rem /* 12px */;
+}
+.space-x-3 > :not([hidden]) ~ :not([hidden]) {
+  --tw-space-x-reverse: 0;
+  margin-right: calc(0.75rem * var(--tw-space-x-reverse));
+  margin-left: calc(0.75rem * calc(1 - var(--tw-space-x-reverse)));
+}
+.w-3 {
+  width: 0.75rem /* 12px */;
+}
+.transform {
+  transform: translate(var(--tw-translate-x), var(--tw-translate-y)) rotate(var(--tw-rotate)) skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y))
+    scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y));
+}
+.transition-all {
+  transition-property: all;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+  transition-duration: 150ms;
+}
+.duration-200 {
+  transition-duration: 200ms;
+}
+.rotate-180 {
+  --tw-rotate: 180deg;
+  transform: translate(var(--tw-translate-x), var(--tw-translate-y)) rotate(var(--tw-rotate)) skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y))
+    scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y));
+}
+.rotate-0 {
+  --tw-rotate: 0deg;
+  transform: translate(var(--tw-translate-x), var(--tw-translate-y)) rotate(var(--tw-rotate)) skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y))
+    scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y));
+}
+</style>
